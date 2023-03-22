d576fedbdd060bc8ffbb4c8b0f75bf27e902090f
sayemon10 <abdulaziz.aziz99@gmail.com>
2023-03-23 01:30:35 +0600

    FIXED

--- app.js
+++ app.js
@@ -5,58 +5,62 @@ const taskList = document.querySelector('#task-list');
 let tasks = [];
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addTask();
+	e.preventDefault();
+	addTask();
 });
 
 function addTask() {
-  if (input.value.trim() === '') {
-    alert('Please enter a task');
-    return;
-  }
-  const task = {
-    id: Date.now(),
-    content: input.value.trim(),
-    completed: false,
-  };
-  tasks.push(task);
-  renderTasks();
-  input.value = '';
+	if (input.value.trim() === '') {
+		alert('Please enter a task');
+		return;
+	}
+	const task = {
+		id: Date.now(),
+		content: input.value.trim(),
+		completed: false,
+	};
+	tasks.push(task);
+	renderTasks();
+	input.value = '';
 }
 
 function renderTasks() {
-  taskList.innerHTML = '';
-  tasks.forEach((task) => {
-    const li = document.createElement('li');
-    li.setAttribute('data-id', task.id);
-    li.classList.add('py-2');
-    li.innerHTML = `
-      <span class="${task.completed ? 'line-through' : ''}">${task.content}</span>
-      <button class="ml-2 text-sm text-red-500 font-medium" data-delete-btn>Delete</button>
-      <button class="ml-2 text-sm text-green-500 font-medium" data-complete-btn>${task.completed ? 'Mark Incomplete' : 'Mark Complete'}</button>
+	taskList.innerHTML = '';
+	tasks.forEach((task) => {
+		const li = document.createElement('li');
+		li.setAttribute('data-id', task.id);
+		li.classList.add('py-2');
+		li.innerHTML = `
+      <span class="${task.completed ? 'line-through' : ''}">${
+			task.content
+		}</span>
+      <button class="ml-2 text-sm text-red-500 font-medium border border-red-500 py-2 px-4 rounded" data-delete-btn>Delete</button>
+      <button class="ml-2 text-sm text-green-500 font-medium border border-green-500 py-2 px-4 rounded" data-complete-btn>${
+			task.completed ? 'Mark Incomplete' : 'Mark Complete'
+		}</button>
     `;
-    taskList.appendChild(li);
-    const deleteBtn = li.querySelector('[data-delete-btn]');
-    deleteBtn.addEventListener('click', (e) => {
-      deleteTask(e.target.closest('li').dataset.id);
-    });
-    const completeBtn = li.querySelector('[data-complete-btn]');
-    completeBtn.addEventListener('click', (e) => {
-      toggleComplete(e.target.closest('li').dataset.id);
-    });
-  });
+		taskList.appendChild(li);
+		const deleteBtn = li.querySelector('[data-delete-btn]');
+		deleteBtn.addEventListener('click', (e) => {
+			deleteTask(e.target.closest('li').dataset.id);
+		});
+		const completeBtn = li.querySelector('[data-complete-btn]');
+		completeBtn.addEventListener('click', (e) => {
+			toggleComplete(e.target.closest('li').dataset.id);
+		});
+	});
 }
 
 function deleteTask(id) {
-  tasks = tasks.filter((task) => task.id !== parseInt(id));
-  renderTasks();
+	tasks = tasks.filter((task) => task.id !== parseInt(id));
+	renderTasks();
 }
 
 function toggleComplete(id) {
-  tasks.forEach((task) => {
-    if (task.id === parseInt(id)) {
-      task.completed = !task.completed;
-    }
-  });
-  renderTasks();
+	tasks.forEach((task) => {
+		if (task.id === parseInt(id)) {
+			task.completed = !task.completed;
+		}
+	});
+	renderTasks();
 }
